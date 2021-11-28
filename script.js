03b929e51a8ef21a86f1f826c48ecc04eb85bd6c
sander <sander.sohngen@usp.br>
2021-11-28 14:21:25 -0300

    created object to hold functions and clean code

--- script.js
+++ script.js
@@ -1,99 +1,119 @@
-initializeWithSize(50);
-
-function initializeWithSize(n) {
-	const container = document.querySelector(".container");
-	while (container.hasChildNodes()) container.removeChild(container.firstChild);
-	for (let i = 0; i < n * n; i++) {
-		const element = document.createElement("div");
-		element.classList.add("dot");
-		container.appendChild(element);
-	}
-	adjustGridSize(n, container);
-}
-
-function adjustGridSize(n, container) {
-	const PixelsPerLine = 50 * 10;
-	const pixelsPerDiv = PixelsPerLine / n;
-	container.style.gridTemplateColumns = `repeat(${n}, ${pixelsPerDiv}px)`;
-	container.style.gridTemplateRows = `repeat(${n}, ${pixelsPerDiv}px)`;
-	const dots = Array.from(container.childNodes);
-	dots.forEach((dot) => {
-		dot.style.height = `${pixelsPerDiv}px`;
-		dot.style.width = `${pixelsPerDiv}px`;
-	});
-}
-
-const randomMode = document.querySelector("#random");
-randomMode.addEventListener("click", () => {
-	hightlightButton(randomMode);
-	const dots = document.querySelectorAll(".dot");
-	dots.forEach((dot) => {
-		dot.addEventListener("mouseover", () => {
-			const hue = Math.floor(Math.random() * 360);
-			dot.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
-		});
-	});
+const buttons = document.querySelector(".sidebar");
+buttons.addEventListener("click", event => {
+	const button = event.target.id;
+	paintDots(button);
 });
 
-const colorMode = document.querySelector("#color");
-colorMode.addEventListener("click", () => {
-	hightlightButton(colorMode);
-	const dots = document.querySelectorAll(".dot");
-	dots.forEach((dot) => {
-		dot.addEventListener("mouseover", () => {
-			const color = "#4F6D7A";
-			dot.style.backgroundColor = color;
-		});
-	});
-});
+const selectedColor = () => {
+	const particularColorMode = document.querySelector("#particularColor");
+	return particularColorMode.value;
+};
 
-const eraserMode = document.querySelector("#eraser");
-eraserMode.addEventListener("click", () => {
-	hightlightButton(eraserMode);
-	const dots = document.querySelectorAll(".dot");
-	dots.forEach((dot) => {
-		dot.addEventListener("mouseover", () => {
-			const color = "#EAEAEA";
-			dot.style.backgroundColor = color;
-		});
-	});
-});
+const predefinedColor = () => "#4F6D7A";
 
-const clearMode = document.querySelector("#clear");
-clearMode.addEventListener("click", () => {
+const randomColor = () => {
+	const hue = Math.floor(Math.random() * 360);
+	return `hsl(${hue}, 100%, 50%)`;
+};
+
+const backgroundColor = () => "#EAEAEA";
+
+const clearAll = () => {
 	const dots = document.querySelectorAll(".dot");
 	dots.forEach((dot) => {
 		const color = "#EAEAEA";
 		dot.style.backgroundColor = color;
 	});
-});
+};
 
-function hightlightButton(button) {
-	const buttons = document.querySelectorAll("button");
-	buttons.forEach((item) => item.classList.remove("selected"));
-	if(button !== null) button.classList.add("selected");
-}
+const modes = {
+	particularColor: selectedColor,
+	color: predefinedColor,
+	random: randomColor,
+	eraser: backgroundColor,
+	clear: clearAll,
+};
 
-const particularColorMode = document.querySelector("#particularColor");
-particularColorMode.addEventListener("click", () => {
+const paintDots = (button) => {
+	if (button === "clear") modes[button]();
+	if (button === "clear" || !modes[button]) return;
+	unhighlightAllButtons();
+	hightlightButton(button);
 	const dots = document.querySelectorAll(".dot");
 	dots.forEach((dot) => {
 		dot.addEventListener("mouseover", () => {
-			const color = particularColorMode.value;
-			dot.style.backgroundColor = color;
+			const color = modes[button];
+			dot.style.backgroundColor = color();
 		});
 	});
-});
+};
+
+function unhighlightAllButtons() {
+	const buttons = document.querySelectorAll("button");
+	buttons.forEach((button) => button.classList.remove("selected"));
+}
+
+function hightlightButton(id) {
+	const button = document.querySelector(`#${id}`);
+	if (id === "particularColor") return;
+	button.classList.add("selected");
+}
 
 const gridSize = document.querySelector("#size");
 gridSize.addEventListener("input", () => {
 	const size = gridSize.value;
 	updateLabel(size);
 	initializeWithSize(size);
-	hightlightButton(null)
+	unhighlightAllButtons();
 });
 
 function updateLabel(size) {
 	const label = document.querySelector("label");
 	label.textContent = `${size}x${size}`;
 }
+
+function initializeWithSize(n) {
+	const container = document.querySelector(".container");
+	removePreviousDots(container);
+	appendNewDots(container, n);
+	adjustGridSize(container, n);
+}
+
+function removePreviousDots(container) {
+	while (container.hasChildNodes())
+		container.removeChild(container.firstChild);
+}
+
+function appendNewDots(container, n) {
+	for (let i = 0; i < n * n; i++) {
+		const element = document.createElement("div");
+		element.classList.add("dot");
+		container.appendChild(element);
+	}
+}
+
+function adjustGridSize(container, n) {
+	const pixelsPerDiv = calculateDivSize(n);
+	formatLineSizes(container, pixelsPerDiv, n);
+	formatDots(container, pixelsPerDiv);
+}
+
+function calculateDivSize(n) {
+	const PixelsPerLine = 50 * 10;
+	return PixelsPerLine / n;
+}
+
+function formatLineSizes(container, pixelsPerDiv, n) {
+	container.style.gridTemplateColumns = `repeat(${n}, ${pixelsPerDiv}px)`;
+	container.style.gridTemplateRows = `repeat(${n}, ${pixelsPerDiv}px)`;
+}
+
+function formatDots(container, pixelsPerDiv) {
+	const dots = container.childNodes;
+	dots.forEach((dot) => {
+		dot.style.height = `${pixelsPerDiv}px`;
+		dot.style.width = `${pixelsPerDiv}px`;
+	});
+}
+
+initializeWithSize(32);
