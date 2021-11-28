61ff1932cb9056c4b5e243e58cd1aea2e656a14e
sander <sander.sohngen@usp.br>
2021-11-28 12:26:35 -0300

    included features to select color and to change dot size, fixing a few other bugs

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const container = document.querySelector(".container");
-for (let i = 0; i < 64 * 64; i++) {
-	const element = document.createElement("div");
-	element.classList.add("dot");
-	container.appendChild(element);
+initializeWithSize(50);
+
+function initializeWithSize(n) {
+	const container = document.querySelector(".container");
+	while (container.hasChildNodes()) container.removeChild(container.firstChild);
+	for (let i = 0; i < n * n; i++) {
+		const element = document.createElement("div");
+		element.classList.add("dot");
+		container.appendChild(element);
+	}
+	adjustGridSize(n, container);
+}
+
+function adjustGridSize(n, container) {
+	const PixelsPerLine = 50 * 10;
+	const pixelsPerDiv = PixelsPerLine / n;
+	container.style.gridTemplateColumns = `repeat(${n}, ${pixelsPerDiv}px)`;
+	container.style.gridTemplateRows = `repeat(${n}, ${pixelsPerDiv}px)`;
+	const dots = Array.from(container.childNodes);
+	dots.forEach((dot) => {
+		dot.style.height = `${pixelsPerDiv}px`;
+		dot.style.width = `${pixelsPerDiv}px`;
+	});
 }
 
 const randomMode = document.querySelector("#random");
 randomMode.addEventListener("click", () => {
-    hightlightButton(randomMode);
+	hightlightButton(randomMode);
 	const dots = document.querySelectorAll(".dot");
 	dots.forEach((dot) => {
-        clearAll(dot);
 		dot.addEventListener("mouseover", () => {
 			const hue = Math.floor(Math.random() * 360);
 			dot.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
@@ -20,10 +37,9 @@ randomMode.addEventListener("click", () => {
 
 const colorMode = document.querySelector("#color");
 colorMode.addEventListener("click", () => {
-    hightlightButton(colorMode);
+	hightlightButton(colorMode);
 	const dots = document.querySelectorAll(".dot");
 	dots.forEach((dot) => {
-        clearAll(dot);
 		dot.addEventListener("mouseover", () => {
 			const color = "#4F6D7A";
 			dot.style.backgroundColor = color;
@@ -33,7 +49,7 @@ colorMode.addEventListener("click", () => {
 
 const eraserMode = document.querySelector("#eraser");
 eraserMode.addEventListener("click", () => {
-    hightlightButton(eraserMode);
+	hightlightButton(eraserMode);
 	const dots = document.querySelectorAll(".dot");
 	dots.forEach((dot) => {
 		dot.addEventListener("mouseover", () => {
@@ -43,21 +59,41 @@ eraserMode.addEventListener("click", () => {
 	});
 });
 
-let clearAll = (dot) => {
-    const color = "#EAEAEA";
-    dot.style.backgroundColor = color;
-}
-
 const clearMode = document.querySelector("#clear");
 clearMode.addEventListener("click", () => {
-    const dots = document.querySelectorAll(".dot");
-    dots.forEach((dot) => clearAll(dot));
-})
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach((dot) => {
+		const color = "#EAEAEA";
+		dot.style.backgroundColor = color;
+	});
+});
 
 function hightlightButton(button) {
-    const buttons = document.querySelectorAll("button");
-    buttons.forEach(item => 
-        item.classList.remove("selected")
-    );
-    button.classList.add("selected");
+	const buttons = document.querySelectorAll("button");
+	buttons.forEach((item) => item.classList.remove("selected"));
+	if(button !== null) button.classList.add("selected");
+}
+
+const particularColorMode = document.querySelector("#particularColor");
+particularColorMode.addEventListener("click", () => {
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach((dot) => {
+		dot.addEventListener("mouseover", () => {
+			const color = particularColorMode.value;
+			dot.style.backgroundColor = color;
+		});
+	});
+});
+
+const gridSize = document.querySelector("#size");
+gridSize.addEventListener("input", () => {
+	const size = gridSize.value;
+	updateLabel(size);
+	initializeWithSize(size);
+	hightlightButton(null)
+});
+
+function updateLabel(size) {
+	const label = document.querySelector("label");
+	label.textContent = `${size}x${size}`;
 }
